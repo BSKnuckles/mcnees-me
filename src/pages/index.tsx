37492db476f4e47e1aafeb6030075c4f6b74218c
Michael McNees <mcnees.michael@gmail.com>
2023-03-21 22:38:12 -0400

    Completed homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,15 @@ export default function Home() {
     <>
       <Head>
         <title>Michael McNees</title>
-        <meta name='description' content="I'm a senior frontend engineer with a passion for " />
+        <meta
+          name='description'
+          content="I'm a senior frontend engineer with a passion for modern tooling and retro video games."
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className='min-h-screen w-full flex flex-col justify-center prose px-8'>
-        <h1 className='text-white text-5xl mb-0'>Hi, I'm Michael McNees.</h1>
+        <h1 className='text-white text-5xl mb-0'>Hi, I'm Michael McNees 🕹</h1>
         <p className='text-gray-300'>
           I build web apps using awesome technologies like{' '}
           <a className='text-sky-400' href='https://nextjs.org/' target='_blank' referrerPolicy='no-referrer'>
@@ -33,6 +36,15 @@ export default function Home() {
         </p>
         <nav className='fixed top-0 right-8'>
           <ul className='list-none flex space-x-4'>
+            <li>
+              <a
+                className='text-gray-300'
+                href='mailto:mcnees.michael@gmail.com'
+                target='_blank'
+                referrerPolicy='no-referrer'>
+                Get In Touch
+              </a>
+            </li>
             <li>
               <a
                 className='text-gray-300'
